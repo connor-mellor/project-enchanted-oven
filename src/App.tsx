9fc5e51a9eacbbd76a1e980572d6510679f866f9
connor-mellor <Connor_Mellor@hotmail.com>
2025-01-24 20:30:16 +0000

    updated c# console app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation'
 import Promo from './components/Promo.tsx'
 import HeroBlock from './components/HeroBlock.tsx'
 import WhatWeDo from './components/WhatWeDo.tsx'
+import Reviews from './components/Reviews.tsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Promo>Get your order in for Valentines Day now! - Feburary 14th</Promo>
       <HeroBlock />
       <WhatWeDo />
+      <Reviews />
     </>
   )
 }
---
+++ src/components/Reviews.tsx
@@ -0,0 +1,30 @@
+const Reviews: React.FC = () => {
+    return (
+        <>
+            <iframe 
+                src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Frachie789%2Fposts%2Fpfbid02yz64tB7y3uMs1Q1MrgZp3WF3eiVZHYQXL8BiPXrJW5kXjH3usc9cnai8UrKswKF7l&show_text=true&width=500" 
+                width="500" 
+                height="169" 
+                style={{ border: "none", overflow: "hidden" }}            
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+            </iframe>
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fconnor.mellor%2Fposts%2Fpfbid0zMAboH3XpVaqE62DS43p1FZcP4XG3VYUGwpQN21Aakxq3v6fe6bmQvmoABNE7ttjl&show_text=true&width=500" 
+                width="500" 
+                height="210" 
+                style={{ border: "none", overflow: "hidden" }}  
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+            </iframe>
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fleah.torrance.54%2Fposts%2Fpfbid021HzMy1vPrprNZgqTvYaHdbeCqbK6Gv9TAsNiNch3MRRjEjc6peb2KnkWeqpqVnpql&show_text=true&width=500" 
+                width="500" 
+                height="188" 
+                style={{ border: "none", overflow: "hidden" }}  
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+            </iframe>
+        </>
+    )
+}
+
+export default Reviews;
