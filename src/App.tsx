1ee1b28163598de05cd0568e381fe43f77bf642a
connor-mellor <Connor_Mellor@hotmail.com>
2025-02-26 01:34:52 +0000

    feat: added dotnet-app-structure file

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import Promo from './components/Promo.tsx'
 import HeroBlock from './components/HeroBlock.tsx'
 import WhatWeDo from './components/WhatWeDo.tsx'
 import Reviews from './components/Reviews.tsx'
-import QuoteForm from './components/QuoteForm..tsx'
+import Gallery from './components/Gallery.tsx'
 import Contact from './components/Contact.tsx'
+import QuoteForm from './components/QuoteForm..tsx'
 import Footer from './components/Footer.tsx'
 
 function App() {
   return (
     <>
-    
       <Navigation />
       <Promo>🐰 Order now in time for the Easter Bunny - April 20th 🐰</Promo>
       <HeroBlock />
       <WhatWeDo />
       <Reviews />
+      <Gallery />
       <Contact />
       <QuoteForm />
       <Footer />
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,37 @@
+const Gallery: React.FC = () => {
+    return (
+        <section className="gallery">
+            <div className="section-title">
+                <h1>Gallery</h1>
+            </div>
+            <div className="gallery-container">
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+                <div className="imageContainer">
+                    <img src={"https://placehold.co/960"} alt="cake" loading="lazy"/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Gallery;
