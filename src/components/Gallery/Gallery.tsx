6f436070348de0eebde9b5ee27e50daef63a0777
connor-mellor <Connor_Mellor@hotmail.com>
2025-03-14 22:37:12 +0000

    added logic for image-zoom

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,16 +1,27 @@
 import images from "../../data/GalleryImages";
 import styles from './Gallery.module.css'
+import ImageZoom from "../ImageZoom/ImageZoom";
+import { useState } from "react";
 
 const Gallery: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [imageData, setImageData] = useState({ imageUrl: "", imageAlt: "" });
+    const setClose = () => setIsOpen(false);
+    const handleClick = (e: React.MouseEventHandler<HTMLDivElement> , src: string, i: number) => {        
+        setImageData({ imageUrl: src, imageAlt: `Position-${i}` })
+        setIsOpen(true)
+    }
+
     return (
         <section className={styles.gallery}>
             <div className={styles.gallery_container}>
                 {images.map((src, i) => (
-                    <div className={styles.image_container} key={i}>
+                    <div className={styles.image_container} key={i} onClick={(e) => handleClick(e, src, i)}>
                         <img src={src} alt={`Position-${i+1}`} loading="lazy"/>
                     </div>
                 ))}
             </div>
+            {isOpen && <ImageZoom imageUrl={imageData.imageUrl} imageAlt={imageData.imageAlt} onClose={setClose} />}
         </section>
     )
 }
